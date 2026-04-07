c7beff5d88c936794e22f6ba95788ce8570d1dcb
Agustin Gonzalez <dtpxl2d@gmail.com>
2026-04-06 22:58:49 -0300

    Subo el primer ejercicio de la clase 01 con nombre ProyectoMundoPC de JavaScript

---
+++ Programacion III/JavaScript/clase01-proyectoMundoPc.js
@@ -0,0 +1,118 @@
+class Orden {
+  static contadorOrdenes = 0;
+
+  constructor() {
+    this._idOrden = ++Orden.contadorOrdenes;
+    this._computadoras = [];
+  }
+
+  agregarComputadora(computadora) {
+    this._computadoras.push(computadora);
+  }
+
+  mostrarOrden() {
+    return `Orden: ${this._idOrden} \n-- Computadoras -- \n${this._computadoras.map(computadora => computadora.toString()).join('\n')}`;
+  }
+}
+
+class Computadora {
+  static contadorComputadora = 0;
+
+  constructor(nombre, monitor, teclado, raton) {
+    this._idComputadora = ++Computadora.contadorComputadora;
+    this._nombre = nombre;
+    this._monitor = monitor;
+    this._teclado = teclado;
+    this._raton = raton;
+  }
+
+  toString() {
+    return `Computadora: ${this._idComputadora} \nNombre: ${this._nombre} \n- Monitor - \n${this._monitor} \n- Teclado - \n${this._teclado} \n- Raton - \n${this._raton}`;
+  }
+}
+
+class Monitor {
+  static contadorMonitor = 0;
+
+  constructor(marca, tamano) {
+    
+    this._idMonitor = ++Monitor.contadorMonitor;
+    this._marca = marca;
+    this._tamano = tamano;
+  }
+
+  get idMonitor() {
+    return this._idMonitor;
+  }
+
+  toString() {
+    return `Monitor: ${this._idMonitor} \nMarca: ${this._marca} \nTamaño: ${this._tamano}`;
+  }
+}
+
+class DispositivoEntrada {
+  constructor(tipoEntrada, marca) {
+    this._tipoEntrada = tipoEntrada;
+    this._marca = marca;
+  }
+
+  get tipoEntrada() {
+    return this._tipoEntrada;
+  }
+  set tipoEntrada(tipoEntrada) {
+    this._tipoEntrada = tipoEntrada;
+  }
+
+  get marca() {
+    return this._marca;
+  }
+  set marca(marca) {
+    this._marca = marca;
+  }
+  
+  toString() {
+    return `Tipo de Entrada: ${this.tipoEntrada} \nMarca: ${this.marca}`;
+  }
+}
+
+class Teclado extends DispositivoEntrada {
+  static contadorTeclado = 0;
+
+  constructor(tipoEntrada, marca) {
+    super(tipoEntrada, marca);
+    this._idTeclado = ++Teclado.contadorTeclado;
+  }
+
+  toString() {
+    return `Teclado: ${this._idTeclado} \nTipo de Entrada: ${this.tipoEntrada} \nMarca: ${this.marca}`;
+  }
+}
+
+class Raton extends DispositivoEntrada {
+  static contadorRaton = 0;
+
+  constructor(tipoEntrada, marca) {
+    super(tipoEntrada, marca);
+    this._idRaton = ++Raton.contadorRaton;
+  }
+
+  toString() {
+    return `Raton: ${this._idRaton} \nTipo de Entrada: ${this.tipoEntrada} \nMarca: ${this.marca}`;
+  }
+}
+
+let raton = new Raton('USB', 'Logitech');
+console.log(raton.toString());
+
+let teclado = new Teclado('USB', 'Microsoft');
+console.log(teclado.toString());
+
+let monitor = new Monitor('Dell', '24 pulgadas');
+console.log(monitor.toString());
+
+let computadora = new Computadora('HP', monitor, teclado, raton);
+console.log(computadora.toString());
+
+let orden = new Orden();
+orden.agregarComputadora(computadora);
+console.log(orden.mostrarOrden());
