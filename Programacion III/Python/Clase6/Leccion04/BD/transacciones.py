02f0c457977beace83e0c09081712d89184017e4
Agustin Gonzalez <dtpxl2d@gmail.com>
2026-05-11 20:42:06 -0300

    Subo clase 6 de Python

---
+++ Programacion III/Python/Clase6/Leccion04/BD/transacciones.py
@@ -0,0 +1,26 @@
+import psycopg2 as bd # Esto es para poder conectarnos a Postgre
+
+conexion = bd.connect(
+  user = 'agus_postgre',
+  password = 'admin',
+  host = '127.0.0.1',
+  port = '5432',
+  database = 'test_bd'
+)
+
+try:
+  # conexion.autocommit = False # esto directamente no debería estar
+  cursor = conexion.cursor()
+  sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s)'
+  valores = ('María', 'Esparza', 'mesparza@gmail.com')
+  cursor.execute(sentencia, valores)
+  conexion.commit() # Hacemos el commit manualmente
+  print('Termina la transacción')
+
+except Exception as e:
+  conexion.rollback()
+  print(f'Ocurrio un error, se hizo un rollback: {e}')
+finally:
+  conexion.close()
+
+# https://www.psycopg.org/docs/usage
--- Programacion III/Python/README.md
+++ Programacion III/Python/README.md
@@ -9,3 +9,12 @@
  - Dia 20/04/2026 - Subimos clase 2 y 3 de Python al repositorio
  - Dia 30/04/2026 - Subimos clase 4 de Python al repositorio
  - Dia 04/05/2026 - Subimos clase 5 de Python al repositorio
+    - Continua usando la carpeta de la clase 4: Leccion04
+    - Se sigue usando el archivo prueba_bd y se crean otros varios mas
+ - Dia 11/05/2026 - Subimos clase 6 de Python al repositorio
+    - Continua usando la carpeta de la clase 4: Leccion04
+    - Los unicos archivos utilizados (modificados, creados, etc) de esta clase son
+      - prueba_bd.py
+      - transacciones.py
+      - transacciones_dos.py
+      - transacciones_tres.py
