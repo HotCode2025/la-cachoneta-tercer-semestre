0778fe5bfb88d68cadd4aae8d6a3deda0a756925
Agustin Gonzalez <dtpxl2d@gmail.com>
2026-05-20 20:11:31 -0300

    Modifico la carpeta practicas para que este mas organizado todo, tambien subo la tarea de area de juegos la cual se llama Avatar

---
+++ Practicas-Area-De-Juegos/Avatar/public/js/avatar.js
@@ -0,0 +1,16 @@
+function seleccionarPersonajeJugador(personaje=null) {
+  console.log(personaje)
+  if (personaje) {
+    alert(`SELECCIONASTE EL PERSONAJE ${personaje}`)
+  } else {
+    alert(`NO HAS SELECCIONADO NINGUN PERSONAJE`)
+  }
+}
+
+let botonPersonajeJugador = document.getElementById('boton-personaje')
+let inputPersonajes = document.getElementsByName('personaje')
+botonPersonajeJugador.addEventListener('click', () => {
+  let personajeSeleccionado = [...inputPersonajes].findIndex((personaje) => personaje.checked)
+  let personaje = document.getElementsByTagName('label')
+  seleccionarPersonajeJugador(personaje[personajeSeleccionado].innerText)}
+)
