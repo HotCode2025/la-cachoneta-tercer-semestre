3b8da38e3caa81e6337786f925602f7091da3618
Emmanuel Yapura <eyapura96@gmail.com>
2026-05-20 20:18:13 -0300

    agrega interfaz grafica del proyecto n reinas

--- Programacion III/JavaScript/Clase7/proyecto_reinas/main.js
+++ Programacion III/JavaScript/Clase7/proyecto_reinas/main.js
@@ -1,112 +1,154 @@
-const N = 10;
+// Elementos DOM
+const $tablero = document.getElementById("tablero");
+const $btnCant = document.getElementById("crear");
+const $input = document.getElementById("cantidad");
+const $textError = document.querySelector(".texto-error");
+
+// Variables globales
+let tablero = [];
+const posiciones = [];
+let N = 0;
 
 // Creamos el tablero vacío
-const tablero = [];
+function crearTablero(N) {
+    tablero = [];
+    for (let i = 0; i < N; i++) {
 
-for (let i = 0; i < N; i++) {
+        let fila = [];
 
-  let fila = [];
+        for (let j = 0; j < N; j++) {
+            fila.push(".");
+        }
 
-  for (let j = 0; j < N; j++) {
-    fila.push(".");
-  }
-
-  tablero.push(fila);
+        tablero.push(fila);
+    }
 }
-
-// Array para guardar posiciones
-const posiciones = [];
 
 // Validamos posiciones para las reinas que se coloquen
 function esSeguro(fila, columna) {
 
-  // Verificamos columna por fila
-  for (let i = 0; i < fila; i++) {
+    // Verificamos columna por fila, movimiento vertical de la reina
+    for (let i = 0; i < fila; i++) {
 
-    if (tablero[i][columna] === "Q") {
-      return false;
+        if (tablero[i][columna] === "R") {
+            return false;
+        }
+
     }
 
-  }
+    // Verificamos diagonal izquierda superior
+    let i = fila - 1;
+    let j = columna - 1;
 
-  // Verificamos diagonal izquierda superior
-  let i = fila - 1;
-  let j = columna - 1;
+    while (i >= 0 && j >= 0) {
 
-  while (i >= 0 && j >= 0) {
+        if (tablero[i][j] === "R") {
+            return false;
+        }
 
-    if (tablero[i][j] === "Q") {
-      return false;
+        i--;
+        j--;
     }
 
-    i--;
-    j--;
-  }
+    // Verificamos diagonal derecha superior
+    i = fila - 1;
+    j = columna + 1;
 
-  // Verificamos diagonal derecha superior
-  i = fila - 1;
-  j = columna + 1;
+    while (i >= 0 && j < N) {
 
-  while (i >= 0 && j < N) {
+        if (tablero[i][j] === "R") {
+            return false;
+        }
 
-    if (tablero[i][j] === "Q") {
-      return false;
+        i--;
+        j++;
     }
 
-    i--;
-    j++;
-  }
-
-  return true;
+    return true;
 }
 
 function resolverNReinas(fila) {
 
-  // Comprobamos si estamos al final
-  if (fila === N) {
-    return true;
-  }
-
-  // Probamos cada columna
-  for (let columna = 0; columna < N; columna++) {
-
-    // Verifica si la posicion es válida
-    if (esSeguro(fila, columna)) {
-
-      // Colocamos reina
-      tablero[fila][columna] = "Q";
-
-      // Guardamos posicion
-      posiciones[fila] = columna;
-
-      // Intentar siguiente fila
-      let solucion = resolverNReinas(fila + 1);
-
-      // Si encontro solucion
-      if (solucion === true) {
+    // Comprobamos si estamos al final
+    if (fila === N) {
         return true;
-      }
-
-      // En caso de que esa reina no se pueda colocar la borramos
-      tablero[fila][columna] = ".";
     }
-  }
 
-  // No encontró solucion
-  return false;
+    // Probamos cada columna
+    for (let columna = 0; columna < N; columna++) {
+
+        // Verifica si la posicion es válida
+        if (esSeguro(fila, columna)) {
+
+            // Colocamos reina
+            tablero[fila][columna] = "R";
+
+            // Guardamos posicion
+            posiciones[fila] = columna;
+
+            // Intentar siguiente fila
+            if (resolverNReinas(fila + 1)) {
+                return true;
+            }
+
+            // En caso de que esa reina no se pueda colocar la borramos
+            tablero[fila][columna] = ".";
+        }
+    }
+
+    // No encontró solucion
+    return false;
 }
 
-// Ejecutamos la funcion principal
-resolverNReinas(0);
+function renderizarTablero() {
 
-// Mostramos tablero
-console.log("- TABLERO FINAL -");
+    // Limpiar
+    $tablero.innerHTML = "";
 
-tablero.forEach(fila => {
-    // Agregamos metodo join para mostrar cada fila con un espacio
-  console.log(fila.join(" "));
+    $tablero.style.gridTemplateColumns = `repeat(${N}, 50px)`;
+    $tablero.style.gridTemplateRows = `repeat(${N}, 50px)`;
+
+    // Crear cuadros
+    for (let fila = 0; fila < N; fila++) {
+
+        for (let columna = 0; columna < N; columna++) {
+
+            const $cuadro = document.createElement("div");
+
+            $cuadro.classList.add("cuadro");
+
+            // Mostrar contenido
+            $cuadro.textContent = tablero[fila][columna];
+
+            // Colores para cada cuadro
+            if ((fila + columna) % 2 === 0) {
+                $cuadro.style.backgroundColor = "white";
+            }
+            else {
+                $cuadro.style.backgroundColor = "gray";
+                $cuadro.style.color = "white";
+            }
+
+            $tablero.appendChild($cuadro);
+        }
+    }
+}
+
+$btnCant.addEventListener("click", () => {
+    N = parseInt($input.value);
+
+    if (N >= 8) {
+        crearTablero(N);
+        resolverNReinas(0);
+        renderizarTablero();
+
+        if (!$textError.classList.contains("hidden")) {
+            $textError.classList.toggle("hidden");
+        }
+    }
+    else {
+        if ($textError.classList.contains("hidden")) {
+            $textError.classList.toggle("hidden");
+        }
+    }
 });
-
-// Mostramos posiciones 
-console.log("\nPosiciones de las reinas:");
-console.log(posiciones);
