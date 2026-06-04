03625bb06b45f0710761c9652854ea00da09a6b3
Agustin Gonzalez <dtpxl2d@gmail.com>
2026-06-03 21:52:03 -0300

    Subo hasta la clase 5 y 6 del Avatar (Clase 9 en total de Programacion III Miercoles), tercera actualizacion del juego Avatar close #29

--- Practicas-Area-De-Juegos/Avatar/public/js/avatar.js
+++ Practicas-Area-De-Juegos/Avatar/public/js/avatar.js
@@ -1,26 +1,75 @@
+const ataques = ["Puño ✊", "Patada 🦶", "Barrida 👣"]
+
 function seleccionarPersonajeJugador(personaje) {
-  let spanPersonajeSeleccionado = document.getElementById('personaje-seleccionado')
+  let pPersonajeSeleccionado = document.getElementById('personaje-seleccionado')
   console.log(personaje)
   if (personaje) {
-    spanPersonajeSeleccionado.innerText = `Has seleccionado a ${personaje}`
+    pPersonajeSeleccionado.innerHTML = `Tu personaje ${personaje} tiene <span>3</span> vidas`
     aleatoria()
   } else {
-    spanPersonajeSeleccionado.innerText = `Debes seleccionar a un personaje`
+    pPersonajeSeleccionado.innerHTML = `Debes elegir un personaje`
   }
+}
+
+function azar(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function calcularAtaqueComputadora() {
+  let ataqueComputadora = ataques[azar(0, 2)]
+
+  return ataqueComputadora
+}
+
+function combatir(ataqueJugador, ataqueComputadora) {
+  if ( ataqueJugador == ataqueComputadora) {
+    return 'GANASTE.'
+  } else if (ataqueJugador == 'Puño ✊' && ataqueComputadora == 'Barrida 👣') {
+    return 'GANASTE.'
+  } else if (ataqueJugador == 'Patada 🦶' && ataqueComputadora == 'Puño ✊') {
+    return 'GANASTE.'
+  } else if (ataqueJugador == 'Barrida 👣' && ataqueComputadora == 'Patada 🦶') {
+    return 'GANASTE.'
+  } else {
+    return 'PERDISTE.'
+  }
+}
+
+function crearMensaje(ataqueJugador, ataqueComputadora, resultado) {
+  let parrafo = document.createElement('p')
+  let sectionMensajes = document.getElementById('mensajes')
+
+  parrafo.className = 'log-ataque'
+  parrafo.innerHTML = `Tu personaje atacó con ${ataqueJugador}, el personaje del enemigo atacó con ${ataqueComputadora}. Resultado: ${resultado}`
+  sectionMensajes.appendChild(parrafo)
 }
 
 function aleatoria() {
   const personajes = ["Zuko 🔥", "Katara 💧", "Aang 🌪️", "Toph 🌱"]
-  const personajeAleatorio = Math.floor(Math.random() * personajes.length)
+  const personajeAleatorio = azar(0,3)
 
   let spanPersonajeComputadora = document.getElementById('personaje-computadora')
-  spanPersonajeComputadora.innerText = `La computadora ha seleccionado a ${personajes[personajeAleatorio]}`
+  spanPersonajeComputadora.innerHTML = `El personaje del enemigo ${personajes[personajeAleatorio]} tiene <span>3</span> vidas`
 }
 
-let botonPersonajeJugador = document.getElementById('boton-personaje')
-let inputPersonajes = document.getElementsByName('personaje')
-botonPersonajeJugador.addEventListener('click', () => {
-  let personajeSeleccionado = [...inputPersonajes].findIndex((personaje) => personaje.checked)
-  let personaje = document.getElementsByTagName('label')[personajeSeleccionado]?.innerText || null
-  seleccionarPersonajeJugador(personaje)}
-)
+function iniciarJuego() {
+  let botonPersonajeJugador = document.getElementById('boton-personaje')
+  let inputPersonajes = document.getElementsByName('personaje')
+  botonPersonajeJugador.addEventListener('click', () => {
+    let personajeSeleccionado = [...inputPersonajes].findIndex((personaje) => personaje.checked)
+    let personaje = document.getElementsByTagName('label')[personajeSeleccionado]?.innerText || null
+    seleccionarPersonajeJugador(personaje)
+  })
+  let botonesAtaques = document.getElementById('botones-ataques')
+
+  botonesAtaques.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      let ataqueJugador = e.target.innerText
+      let ataqueComputadora = calcularAtaqueComputadora()
+      let resultado = combatir(ataqueJugador, ataqueComputadora)
+      crearMensaje(ataqueJugador, ataqueComputadora, resultado)
+    }
+  })
+}
+
+iniciarJuego()
