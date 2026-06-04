4c0ab578ea042e93eff39c82672cefdc3bf0b110
Agustin Gonzalez <dtpxl2d@gmail.com>
2026-06-03 22:16:19 -0300

    Le pongo comentarios a la tercera actualizacion del juego del Avatar

--- Practicas-Area-De-Juegos/Avatar/public/js/avatar.js
+++ Practicas-Area-De-Juegos/Avatar/public/js/avatar.js
@@ -1,5 +1,21 @@
+/* Array de los ataques, sirve para cuando necesito generar un ataque random para la computadora */
 const ataques = ["Puño ✊", "Patada 🦶", "Barrida 👣"]
 
+/* la funcion con la formula milagrosa para sacar numeros aleatorios */
+function azar(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+/* Esta funcion sirve para que la computadora eliga un personaje aleatorio, el nombre de la funcion es medio ambiguo igual, espero que se entienda con este comentario jaja*/
+function aleatoria() {
+  const personajes = ["Zuko 🔥", "Katara 💧", "Aang 🌪️", "Toph 🌱"]
+  const personajeAleatorio = azar(0,3)
+
+  let spanPersonajeComputadora = document.getElementById('personaje-computadora')
+  spanPersonajeComputadora.innerHTML = `El personaje del enemigo ${personajes[personajeAleatorio]} tiene <span>3</span> vidas`
+}
+
+/* En esta funcion modificamos los elementos p del html para mostrar el personaje que elegimos o si no elegimos ningun personaje */
 function seleccionarPersonajeJugador(personaje) {
   let pPersonajeSeleccionado = document.getElementById('personaje-seleccionado')
   console.log(personaje)
@@ -11,16 +27,14 @@ function seleccionarPersonajeJugador(personaje) {
   }
 }
 
-function azar(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
+/* Funcion para calcular de forma aleatoria el ataque de la computadora*/
 function calcularAtaqueComputadora() {
   let ataqueComputadora = ataques[azar(0, 2)]
 
   return ataqueComputadora
 }
 
+/* Logica de combate, igualito a la logica del piedra papel y tijeras, pero modificado para este juego, debemos agregar reglas luego para que se entienda cuando se gana o cuando se pierde */
 function combatir(ataqueJugador, ataqueComputadora) {
   if ( ataqueJugador == ataqueComputadora) {
     return 'GANASTE.'
@@ -35,6 +49,7 @@ function combatir(ataqueJugador, ataqueComputadora) {
   }
 }
 
+/* Este es la funcion para mostrar el log del combate, crea un elemento html con contenido (tambien agregue un classname para modificarlo con css para que se vea bonito en pantalla), luego lo agregamos a un section usando appendChild */
 function crearMensaje(ataqueJugador, ataqueComputadora, resultado) {
   let parrafo = document.createElement('p')
   let sectionMensajes = document.getElementById('mensajes')
@@ -44,24 +59,20 @@ function crearMensaje(ataqueJugador, ataqueComputadora, resultado) {
   sectionMensajes.appendChild(parrafo)
 }
 
-function aleatoria() {
-  const personajes = ["Zuko 🔥", "Katara 💧", "Aang 🌪️", "Toph 🌱"]
-  const personajeAleatorio = azar(0,3)
 
-  let spanPersonajeComputadora = document.getElementById('personaje-computadora')
-  spanPersonajeComputadora.innerHTML = `El personaje del enemigo ${personajes[personajeAleatorio]} tiene <span>3</span> vidas`
-}
-
+/* Esta funcion la creo el profe en clase, y como el codigo que he hecho ha sido diferente al de las clases para ir aprendiendo, supuse que asi deberia de estar bien, el codigo funciona. Imagino que luego podemos usar el iniciarJuego() dentro de una funcion reiniciar() para bueno, reiniciar el juego obviamente jaja */
 function iniciarJuego() {
   let botonPersonajeJugador = document.getElementById('boton-personaje')
   let inputPersonajes = document.getElementsByName('personaje')
+  /* Uso el boton para escuchar el click, luego chequeo (usando inputPersonajes y checked) cual fue el boton seleccionado, o en papel, el personaje seleccionado. Aca esta basicamente la inicializacion de la logica detras del saber que personaje eligio el jugador y generar un personaje aleatorio para la computadora*/
   botonPersonajeJugador.addEventListener('click', () => {
     let personajeSeleccionado = [...inputPersonajes].findIndex((personaje) => personaje.checked)
     let personaje = document.getElementsByTagName('label')[personajeSeleccionado]?.innerText || null
     seleccionarPersonajeJugador(personaje)
   })
-  let botonesAtaques = document.getElementById('botones-ataques')
 
+  /* Esta es la inicializacion de la logica del combate, utilizando delegacion de eventos */
+  let botonesAtaques = document.getElementById('botones-ataques')
   botonesAtaques.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
       let ataqueJugador = e.target.innerText
